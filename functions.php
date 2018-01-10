9fceee1ce153d9a8328092ce8a74b1a4878e5cae
Aitch <dev-aitch@hotmail.com>
2018-01-10 18:17:58 +0100

    Enqueued Child style theme

--- functions.php
+++ functions.php
@@ -1,6 +1,22 @@
 <?php 
 
-	/**
-	 * REGISTER SCRIPTS
-	 */
+///////////////////////////////////
+//REGISTER & INCLUDES SCRIPTS // //
+///////////////////////////////////
 	include_once("page-templates/inc-header.php");
+
+	/**
+	 * Enqueue Child Theme Style
+	 */
+	function linares_enqueue_styles(){
+	    // SweetTooth Style
+	    $parent_style = 'parent-style'; 
+
+	    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+	    wp_enqueue_style( 'child-style',
+	        get_stylesheet_directory_uri() . '/style.css',
+	        array( $parent_style ),
+	        wp_get_theme()->get('Version')
+	    );
+	}
+	add_action( 'wp_enqueue_scripts', 'linares_enqueue_styles' );
