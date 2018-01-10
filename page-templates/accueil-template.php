280deaf2c23bc7c6d2a49ec6f1e7eddf26333e02
Aitch <dev-aitch@hotmail.com>
2018-01-10 18:24:38 +0100

    added speed-nav tabs

--- page-templates/accueil-template.php
+++ page-templates/accueil-template.php
@@ -1,24 +1,29 @@
 <?php 
 /*
-Template Name: Accueil Linares
+Template Name: Linares Accueil
 */ 
 ?>
 <?php 
 	get_header();
  ?>
  <div class="container">
- 	<div class="row">
- 		<div class="col-md-6">
+ 	<section class="row body-wrapper">
+ 		<div class="col-md-6 body-thumbnail">
  			<?php the_post_thumbnail(); ?>
  		</div>
- 		<div class="col-md-6">
+ 		<div class="col-md-6 body-content">
  			<?php 
  				while (have_posts()) : 
  					the_post();
- 					$contents = get_the_content();
- 					print_r($contents);
+ 					$contents = the_content();
+ 					echo $contents;
  				endwhile;
  			 ?>
+ 			 <ul class="list-inline cta-list-nav">
+ 			 	<li>Nos Prestations</li>
+ 			 	<li>Nos Engagements</li>
+ 			 	<li>Contactez-Nous</li>
+ 			 </ul>
  		</div>
- 	</div>
+ 	</section>
  </div>
