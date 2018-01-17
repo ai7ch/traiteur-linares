4736bb8ba3345e66783c6c29c2fa68913984a433
Aitch <dev-aitch@hotmail.com>
2018-01-17 22:46:27 +0100

    Hoooking up child-style & other dependecies

--- functions.php
+++ functions.php
@@ -3,20 +3,59 @@
 ///////////////////////////////////
 //REGISTER & INCLUDES SCRIPTS // //
 ///////////////////////////////////
-	include_once("page-templates/inc-header.php");
-
 	/**
-	 * Enqueue Child Theme Style
-	 */
-	function linares_enqueue_styles(){
-	    // SweetTooth Style
-	    $parent_style = 'parent-style'; 
+	 * Child Theme Style*/
+	function linares_css(){
+	    // Dependencies
+	    $bootstrap_css 	= "bootstrap-css";
+	    //$parent_style 	= "parent-style";
 
-	    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
-	    wp_enqueue_style( 'child-style',
-	        get_stylesheet_directory_uri() . '/style.css',
-	        array( $parent_style ),
-	        wp_get_theme()->get('Version')
+	    //Enqueue Frameworks
+	    wp_enqueue_style("bootstrap-css", 
+	    	"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css", 
+	    	array(),
+	    	"3.3.7", 
+	    	"all" //media
+	    );
+	    
+	    //wp_enqueue_style("parent-style", get_template_directory_uri() . "/style.css" );
+	    wp_enqueue_style( "child-style",
+	        get_stylesheet_directory_uri() . "/style.css",
+	        array(),//$parent_style
+	        "0.1",
+	        "all"
 	    );
 	}
-	add_action( 'wp_enqueue_scripts', 'linares_enqueue_styles' );
+
+	/**
+	 * Child Theme Scripts*/
+	function linares_js(){
+		//Dependencies
+		$bootstrap_js = "bootstrap-js";
+		wp_register_script($bootstrap_js,
+			"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js",
+			array(), 
+			"3.3.7", 
+			true 
+		);
+
+		wp_register_script("linares_style_javascript",
+			get_stylesheet_directory_uri() . "/assets/js/style.js",
+			array(
+				"jquery", 
+				$bootstrap_js
+			), 
+			"0.1",
+			true
+		);
+
+		wp_enqueue_script("linares_style_javascript" );
+		wp_enqueue_script($bootstrap_js);
+	}
+
+	add_action( "wp_enqueue_scripts", "linares_css" );
+	add_action( "wp_enqueue_scripts", "linares_js" );
+
+////////////////////
+//UNREGISTER SUFF///
+////////////////////
