a20ecedfeaef6786e84927f361083debd9b94216
Aitch <dev-aitch@hotmail.com>
2018-02-17 01:55:45 +0100

    added google map and other optimizations

--- page-templates/contact-template.php
+++ page-templates/contact-template.php
@@ -29,21 +29,28 @@ Template Name: Linares Contact
  					<div class="col-md-6">
 		 				<div class="text-contact">
 		 					<h3>Traiteur Linares</h3>
-		 					<p><?php echo $contact_info_fields["texte_presentation"]?></p>
+		 					<?php if(!empty($contact_info_fields["texte_presentation"])) :?>
+		 						<p><?php echo $contact_info_fields["texte_presentation"]?></p>
+		 					<?php endif; ?>
 		 				</div>
 		 				<div id="address">
 		 					<ul class="list-unstyled">
 		 						<li><?php echo $contact_info_fields["adresse"]["ville"] ?></li>
 		 						<li><?php echo $contact_info_fields["adresse"]["numero_rue"] ?></li>
 		 						<li><?php echo $contact_info_fields["adresse"]["code_postal"] ?></li>
-		 						<li><?php echo $contact_info_fields["adresse"]["email"] ?></li>
-		 						<li><?php echo $contact_info_fields["adresse"]["phone"] ?></li>
+		 						<li><a href="mailto:<?php echo $contact_info_fields["adresse"]["email"] ?>"><?php echo $contact_info_fields["adresse"]["email"] ?></a></li>
+		 						<li itemprop="telephone"><a href="tel:<?php echo $contact_info_fields["adresse"]["phone"] ?>"><?php echo $contact_info_fields["adresse"]["phone"] ?></a></li>
 		 					</ul>
 		 				</div>
  					</div>
  					<div class="col-md-6">	 						
 		 				<div id="form">
 		 					<?php echo do_shortcode($contact_info_fields["form"]) ?>
+		 				</div>
+ 					</div>
+ 					<div class="col-md-12"> 						
+		 				<div id="map">
+		 					<iframe src="https://www.google.com/maps/embed/v1/place?key=AIzaSyDo3A64fY98VvBEsupGXKLHxdQuRioHuiU&q=2+Chemin+du+Moulinet%2C+13015+Marseille&attribution_source=Google+Maps+Embed+API&attribution_web_url=http://www.traiteur-linares.fr" frameborder="0"></iframe>
 		 				</div>
  					</div>
  				</div> <!--#infos-contact-wrapper -->
