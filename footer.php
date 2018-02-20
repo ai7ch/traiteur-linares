7117bcb54a478f8bcb304ae908ff124fbc4b1a57
Aitch <dev-aitch@hotmail.com>
2018-02-20 22:21:44 +0100

    created child-footer page

---
+++ footer.php
@@ -0,0 +1,7 @@
+<div class="container" id="footer">
+	<div class="row">
+		<div class="col-md-8">
+			<p>Traiteur Linares - 2 chemin du Moulinet, 13015 MARSEILLE - 06 29 17 81 57 -  contact@traiteur-linares.fr</p>
+		</div>
+	</div>
+</div>
