d1636acef496010a3919cacff5f71b13f51d2961
Aitch <dev-aitch@hotmail.com>
2018-02-20 23:29:12 +0100

    added footer contact and Link Mention legales

--- footer.php
+++ footer.php
@@ -3,5 +3,10 @@
 		<div class="col-md-8">
 			<p>Traiteur Linares - 2 chemin du Moulinet, 13015 MARSEILLE - 06 29 17 81 57 -  contact@traiteur-linares.fr</p>
 		</div>
+		<div class="col-md-4">
+			<p>
+				<a href="#MENTIONS LEGALES" onclick="return false;">MENTIONS LEGALES</a>
+			</p>
+		</div>
 	</div>
 </div>
